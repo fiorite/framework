44e3abe7099dec758210cb27039e276885f30c6e
Yevhen Melnychenko <eugen.melnychenko@gmail.com>
2024-10-16 00:08:34 +0300

    changed configuration, so vite builds lib isntead esmodule

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,22 @@ export default defineConfig({
   build: {
     outDir: __dirname + '/dist',
     ssr: true,
-    minify: true,
-    assetsDir: 'common',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
+    lib: {
+      entry: {
         'index': __dirname + '/src/index.ts',
         'nodejs': __dirname + '/src/nodejs/index.ts',
         'sqlite3': __dirname + '/src/sqlite3/index.ts',
         'vite': __dirname + '/src/vite/index.ts',
         'firestore': __dirname + '/src/firestore/index.ts',
       },
+    },
+    minify: true,
+    assetsDir: 'common',
+    emptyOutDir: true,
+    rollupOptions: {
       external: [...nodeJsExternal, 'sqlite3', 'firebase-admin/firestore', 'vite', '@rollup/plugin-swc', '@swc/types'],
     }
   },
-  plugins: [
-    typescript()
-  ],
+  plugins: [typescript()],
 });
 
